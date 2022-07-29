f3b8c9bf386ea17614a8bb99112ce88fad4e9f25
GODZHEERA <volkins@mail.ru>
2022-07-30 02:01:54 +0300

    Task completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string [] array = new string[5] {"cat", "dog", "fridge", "window", "sun"};
+int CharsCount = 3;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine("==========");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= CharsCount)
+    {
+        Console.WriteLine(array[i]);
+    }
+
+}
